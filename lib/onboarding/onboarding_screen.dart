e5e98c40520d151c7610ed8d828cd47996deec2d
SalahHassan202 <salahhassan66190@gmail.com>
2026-01-31 12:19:51 +0200

    finish onboarding screen

--- lib/onboarding/onboarding_screen.dart
+++ lib/onboarding/onboarding_screen.dart
@@ -1,31 +1,32 @@
+import 'package:doctor_hun_app/onboarding/onboarding_item.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatelessWidget {
-  final String image;
-  final String title;
-  final String desc;
-  const OnboardingScreen({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.desc,
-  });
+  const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Positioned(
-        top: -150,
-        right: -100,
-        child: Container(
-          width: 350,
-          height: 350,
-          decoration: const BoxDecoration(
-            color: Color(0xff16C79A),
-            shape: BoxShape.circle,
-          ),
+    return PageView(
+      children: [
+        OnboardingItem(
+          image: "assets/on_boarding_image_1.png",
+          title: "Find Trusted Doctors",
+          desc:
+              "Contrary to popular belief, Lorem Ipsum is not\n simply random text. It has roots in a piece of it\n over 2000 years old.",
         ),
-      ),
+        OnboardingItem(
+          image: "assets/on_boarding_image_2.png",
+          title: "Choose Best Doctors",
+          desc:
+              "Contrary to popular belief, Lorem Ipsum is not\n simply random text. It has roots in a piece of it\n over 2000 years old.",
+        ),
+        OnboardingItem(
+          image: "assets/on_boarding_image_3.png",
+          title: "Easy Appointments",
+          desc:
+              "Contrary to popular belief, Lorem Ipsum is not\n simply random text. It has roots in a piece of it\n over 2000 years old.",
+        ),
+      ],
     );
   }
 }
