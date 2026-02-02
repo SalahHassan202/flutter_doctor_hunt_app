418e62f9c99f9bcc7504188b03606c511e36bf44
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-02 12:38:42 +0200

    update live ordered section

--- lib/home_Screen/widgets/live_dector_section.dart
+++ lib/home_Screen/widgets/live_dector_section.dart
@@ -1,57 +1,36 @@
+import 'package:doctor_hun_app/home_Screen/widgets/live_doctor_card.dart';
 import 'package:flutter/material.dart';
 
-class LiveDector extends StatelessWidget {
-  const LiveDector({super.key});
+class LiveDectorSection extends StatelessWidget {
+  const LiveDectorSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 160,
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        itemBuilder: (context, index) {
-          return Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(16),
-                child: Image.asset(
-                  "assets/ive_doctor_0.png",
-                  width: 120,
-                  height: 160,
-                  fit: BoxFit.fill,
-                ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Live Doctors",
+            style: TextStyle(fontWeight: FontWeight.w900),
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            height: 120,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: 3,
+              separatorBuilder: (_, __) => const SizedBox(width: 12),
+              itemBuilder: (_, i) => LiveDoctorCard(
+                live: true,
+                // لو عندك صور حطها هنا:
+                // imageAsset: "assets/images/live_$i.png",
+                imageAsset: null,
               ),
-              Positioned(
-                top: 8,
-                left: 8,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 4,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: const Text(
-                    "LIVE",
-                    style: TextStyle(color: Colors.white, fontSize: 10),
-                  ),
-                ),
-              ),
-              const Positioned.fill(
-                child: Icon(
-                  Icons.play_circle_fill,
-                  color: Colors.white,
-                  size: 40,
-                ),
-              ),
-            ],
-          );
-        },
-        separatorBuilder: (_, __) => const SizedBox(width: 12),
-        itemCount: 3,
+            ),
+          ),
+        ],
       ),
     );
   }
