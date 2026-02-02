bb7101b82f275399c5fc036ba350f098ccbc3e42
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-02 11:48:02 +0200

    updete 3 files

--- lib/home_Screen/widgets/home_header.dart
+++ lib/home_Screen/widgets/home_header.dart
@@ -6,49 +6,59 @@ class HomeHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
+    return Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.all(20),
 
-      width: double.infinity,
-      height: 156,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Color(0xff16C79A), Color(0xff0FB9B1)],
-        ),
-        borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          width: double.infinity,
+          height: 156,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xff16C79A), Color(0xff0FB9B1)],
+            ),
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              SizedBox(height: 12),
-              Text(
-                "Hi Handwerker!",
-                style: GoogleFonts.rubik(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w100,
-                  color: Colors.white,
-                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 12),
+                  Text(
+                    "Hi Handwerker!",
+                    style: GoogleFonts.rubik(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w100,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Find Your Doctor",
+                    style: GoogleFonts.rubik(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 8),
-              Text(
-                "Find Your Doctor",
-                style: GoogleFonts.rubik(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+              CircleAvatar(
+                radius: 30,
+                backgroundImage: AssetImage("assets/person.png"),
               ),
             ],
           ),
-          CircleAvatar(
-            radius: 30,
-            backgroundImage: AssetImage("assets/person.png"),
-          ),
-        ],
-      ),
+        ),
+        Positioned(
+          left: 16,
+          right: 16,
+          bottom: -10,
+          child: SearchBar(hintText: "Search...", onChanged: (v) {}),
+        ),
+      ],
     );
   }
 }
--- lib/home_Screen/widgets/search_bar.dart
+++ lib/home_Screen/widgets/search_bar.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
 
-class SearchBarWidget extends StatelessWidget {
-  const SearchBarWidget({super.key});
+class SearchBar extends StatelessWidget {
+  final String hint;
+  final ValueChanged<String> onChanged;
+
+  const SearchBar({super.key, required this.hint, required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(20),
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: [BoxShadow(blurRadius: 10, color: Colors.black12)],
-      ),
-      child: const TextField(
-        decoration: InputDecoration(
-          hintText: "Search...",
-          border: InputBorder.none,
-          icon: Icon(Icons.search),
-          suffixIcon: Icon(Icons.close),
+    return Material(
+      elevation: 6,
+      shadowColor: Colors.black12,
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        height: 48,
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        decoration: BoxDecoration(color: Colors.white),
+        child: Row(
+          children: [
+            const Icon(Icons.search, color: Color(0xFF8A8A8A)),
+            const SizedBox(width: 10),
+            Expanded(
+              child: TextField(
+                onChanged: onChanged,
+                decoration: InputDecoration(
+                  hintText: hint,
+                  suffixIcon: Icon(Icons.search),
+
+                  hintStyle: const TextStyle(color: Color(0xFFB0B0B0)),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const Icon(Icons.close, color: Color.fromARGB(255, 0, 0, 0)),
+          ],
         ),
       ),
     );
