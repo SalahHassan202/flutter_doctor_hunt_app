2bcaf8ed82cb1d675cedeb6734186a474cae0d4d
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-02 10:13:41 +0200

    update home screen

--- lib/home_Screen/home_screen.dart
+++ lib/home_Screen/home_screen.dart
@@ -5,6 +5,30 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favorite",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.read_more_sharp),
+            label: "Read",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
+        ],
+      ),
+
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
