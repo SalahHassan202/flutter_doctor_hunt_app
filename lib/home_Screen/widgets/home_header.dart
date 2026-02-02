8d45a02a01f3120b4ee35c3268aac3e1b326f992
SalahHassan202 <salahhassan66190@gmail.com>
2026-02-02 10:28:19 +0200

    added home_header_file

---
+++ lib/home_Screen/widgets/home_header.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 156,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xff16C79A), Color(0xff0FB9B1)],
+        ),
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hi Handwerker!",
+                style: GoogleFonts.rubik(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w100,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                "Find Your Doctor",
+                style: GoogleFonts.rubik(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          CircleAvatar(
+            radius: 30,
+            backgroundImage: AssetImage("assets/person.png"),
+          ),
+        ],
+      ),
+    );
+  }
+}
